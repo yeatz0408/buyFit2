5ecf0be18292f9b4bc46d98c125702903a6398aa
unknown <yeatz0408@gmail.com>
2023-04-17 20:12:11 +0900

    configure MySQL connection

---
+++ buyFit2backend/buyFit2backend/src/main/java/com/gmail/yeatz0408/buyFit2backend/controllers/PageController.java
@@ -0,0 +1,17 @@
+package com.gmail.yeatz0408.buyFit2backend.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/pages", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class PageController {
+
+    // @GetMapping
+    // public List<Page> getAll() {
+
+    // }
+    
+}
--- buyFit2backend/buyFit2backend/src/main/java/com/gmail/yeatz0408/buyFit2backend/repositories/PageRepository.java
+++ buyFit2backend/buyFit2backend/src/main/java/com/gmail/yeatz0408/buyFit2backend/repositories/PageRepository.java
@@ -1,7 +1,5 @@
 package com.gmail.yeatz0408.buyFit2backend.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,11 +8,11 @@ import com.gmail.yeatz0408.buyFit2backend.entities.Page;
 @Repository
 public interface PageRepository extends JpaRepository<Page, Long> {
 
-    Page findBySlug(String slug);
+    // Page findBySlug(String slug);
 
-    Page findBySlugAndIdNot(Long id, String slug);
+    // Page findBySlugAndIdNot(Long id, String slug);
 
-    List<Page> findAllByOrderBySortingAsc();
+    // List<Page> findAllByOrderBySortingAsc();
     
 }
 
