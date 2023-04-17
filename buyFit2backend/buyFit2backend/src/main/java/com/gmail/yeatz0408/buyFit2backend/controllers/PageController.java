e27f084e1c3a6839c60c47ceec450aaf1d161a45
unknown <yeatz0408@gmail.com>
2023-04-17 20:25:30 +0900

    add get all mapping for pages

--- buyFit2backend/buyFit2backend/src/main/java/com/gmail/yeatz0408/buyFit2backend/controllers/PageController.java
+++ buyFit2backend/buyFit2backend/src/main/java/com/gmail/yeatz0408/buyFit2backend/controllers/PageController.java
@@ -1,17 +1,30 @@
 package com.gmail.yeatz0408.buyFit2backend.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gmail.yeatz0408.buyFit2backend.entities.Page;
+import com.gmail.yeatz0408.buyFit2backend.repositories.PageRepository;
+
 @RestController
-@RequestMapping(path = "/pages", produces = "application/json")
+@RequestMapping(path = "/pages")
 @CrossOrigin(origins = "*")
 public class PageController {
 
-    // @GetMapping
-    // public List<Page> getAll() {
+    @Autowired
+    private PageRepository pageRepo;
 
-    // }
+    @GetMapping("/all")
+    public Iterable<Page> getAll() {
+
+        List<Page> pages = pageRepo.findAll();
+
+        return pages;
+    }
     
 }
