f9aff3c0a59393e91efe86b7f7a449f1490a3a92
unknown <yeatz0408@gmail.com>
2023-04-20 06:40:49 +0900

    add delete confirmation

--- buy-fit2-frontend/src/admin/pages/Index.js
+++ buy-fit2-frontend/src/admin/pages/Index.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { confirmAlert } from 'react-confirm-alert';
 import axios from 'axios';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 export default function Index() {
 
@@ -14,20 +17,32 @@ export default function Index() {
         setPages(result.data);
     };
 
-    const noPages = () => {
-        if (pages.length != 0 ) {
-            
-        } 
-        
-        return null;
+    const deletePage = async (id) => {
+
+        await axios.delete(`http://localhost:8080/admin/pages/delete/${id}`);
+        loadPages();
     }
 
+    const submitDelete = (id) => {
+        confirmAlert({
+            message: '削除しますか？',
+            buttons: [
+              {
+                label: 'はい',
+                onClick: () => deletePage(id)
+              },
+              {
+                label: 'いいえ',
+              }
+            ]
+          });
+    }
 
     return (
         <div className="container">
             <h1 className="display-2">ページ</h1>
             <a href="/admin/pages/add" className="btn btn-primary mb-5">追加</a>
-            
+
             {pages.length == 0 &&
                 <h4>ページを追加してください。</h4>
             }
@@ -37,16 +52,20 @@ export default function Index() {
                     <tr>
                         <th>タイトル</th>
                         <th>スラグ</th>
-                        <th>変更</th>
-                        <th>削除</th>
+                        <th></th>
                     </tr>
                     {
                         pages.map((page, index) => (
                             <tr key={page.id}>
                                 <td>{page.title}</td>
                                 <td>{page.slug}</td>
-                                <td><a>変更</a></td>
-                                <td><a>削除</a></td>
+                                <td><Link
+                                    to={`/admin/pages/edit/${page.id}`}
+                                    className="btn btn-outline-primary mx-2">変更</Link></td>
+                                <button
+                                    className="btn btn-danger mx-2"
+                                    onClick={() => submitDelete(page.id)}
+                                >削除</button>
                             </tr>
 
                         ))
