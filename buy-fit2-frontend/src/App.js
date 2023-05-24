41dcb8ee17a59bf467a2c2f5334d2115269709ac
unknown <yeatz0408@gmail.com>
2023-05-24 12:44:04 +0900

    add SpinnerLoading oktaConfig OktaSignInWidget

--- buy-fit2-frontend/src/App.js
+++ buy-fit2-frontend/src/App.js
@@ -11,15 +11,33 @@ import CategoryEdit from './admin/categories/CategoryEdit'
 import ProductIndex from './admin/products/ProductIndex'
 import ProductAdd from './admin/products/ProductAdd'
 import ProductEdit from './admin/products/ProductEdit'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import Page from './user/Page';
 import Product from './user/Product';
 import { Categories } from './util/Categories';
+import { oktaConfig } from './lib/oktaConfig';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { Security, LoginCallback } from '@okta/okta-react';
+import LoginWidget  from './Auth/LoginWidget'
 
-function App() {
+const oktaAuth = new OktaAuth(oktaConfig);
+
+export const App = () => {
+
+  const customAuthHandler = () => {
+    history.push('/login')
+  }
+
+  const history = useHistory();
+
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+  };
+
   return (
     <div className="App">
       <Router>
+        <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
         <Navbar admin={true} />
 
         <div className="container-fluid">
@@ -46,14 +64,15 @@ function App() {
                 <Route exact path="/pages/:slug" element={<Page/>}/>
                 <Route exact path="/products" element={<Product/>}/>
                 <Route exact path="/products/:slug" element={<Product/>}/>
+
+                <Route path="/login" render={() => <LoginWidget config={oktaConfig} />} />
+                <Route path='/login/callback' component={LoginCallback} />
               </Routes>
             </div>
           </div>
         </div>
-
+        </Security>
       </Router>
     </div>
   );
 }
-
-export default App;
---
+++ buy-fit2-frontend/src/Auth/LoginWidget.js
@@ -0,0 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+import { useOktaAuth } from '@okta/okta-react';
+import { SpinnerLoading } from '../util/SpinnerLoading';
+import OktaSignInWidget from './OktaSignInWidget';
+
+const LoginWidget = ({ config }) => {
+    const { oktaAuth, authState } = useOktaAuth();
+    const onSuccess = (tokens) => {
+        oktaAuth.handleLoginRedirect(tokens);
+    };
+
+    const onError = (err) => {
+        console.log('Sign in error: ', err);
+    }
+
+    const navigate = useNavigate();
+
+    if (!authState) {
+        return (
+            <SpinnerLoading/>
+        );
+    }
+
+    return authState.isAuthenticated ?
+    navigate()
+    :
+    <OktaSignInWidget config={config} onSuccess={onSuccess} onError={onError}/>
+};
+
+export default LoginWidget;
